4ad54afa0ac692e73104dafe42b9101d07c81847
goksunonal <32015340+goksunonal@users.noreply.github.com>
2018-10-31 21:01:46 +0300

    Adding more classes (84c72fe)

--- photo_album/settings_local.py
+++ photo_album/settings_local.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
+    'DEFAULT_AUTHENTICATION_CLASSES':[],
+    'DEFAULT_PERMISSION_CLASSES':[]
 }
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
