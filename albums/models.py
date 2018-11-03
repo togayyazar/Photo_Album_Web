7f9421bd27126cc8782b12a030ec968685580c65
goksunonal <32015340+goksunonal@users.noreply.github.com>
2018-11-03 15:53:29 +0300

    Update models.py

--- albums/models.py
+++ albums/models.py
@@ -6,8 +6,8 @@ from rest_framework import serializers
 class Album(models.Model):
     title = models.CharField(max_length=50, blank=False)
     owner = models.CharField(max_length=50, blank=False)
-    date = models.DateTimeField(default=datetime.now, blank=True)
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    
 class Photo(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE, blank=False)
     photo = models.ImageField(upload_to='albums/static/images/')
