e71e139e111b28f4a2359f8f4208273c08cbb326
Furkan YILMAZ <f.yilmaz@etu.edu.tr>
2018-11-03 17:36:04 +0300

    update photo path #8

--- albums/models.py
+++ albums/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from datetime import datetime
 from rest_framework import serializers
+import uuid
+import os
+
+
+def get_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join('albums/static/images', filename)
 
 
 class Album(models.Model):
@@ -8,15 +16,18 @@ class Album(models.Model):
     owner = models.CharField(max_length=50, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
+
 class Photo(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE, blank=False)
-    photo = models.ImageField(upload_to='albums/static/images/')
+    photo = models.ImageField(upload_to=get_file_path)
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
 
 class PhotoSerializers(serializers.ModelSerializer):
     class Meta:
             model = Photo
             fields = '__all__'
+
 
 class AlbumSerializer(serializers.ModelSerializer):
     class Meta:
