7a87dce50a1cbdbcab7271ec7b7de8cd6fdb1619
goksunonal <32015340+goksunonal@users.noreply.github.com>
2018-11-03 16:31:45 +0300

    Adding Rest Framework to models (Issue #9)

--- albums/models.py
+++ albums/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import datetime
+from rest_framework import serializers
 
 
 class Album(models.Model):
@@ -6,8 +8,17 @@ class Album(models.Model):
     owner = models.CharField(max_length=50, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
-
 class Photo(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE, blank=False)
-    photo = models.FileField(upload_to='photos/')
+    photo = models.ImageField(upload_to='albums/static/images/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+class PhotoSerializers(serializers.ModelSerializer):
+    class Meta:
+            model = Photo
+            fields = '__all__'
+
+class AlbumSerializer(serializers.ModelSerializer):
+    class Meta:
+            model = Album
+            fields = '__all__'
--- photo_album/settings_local.py
+++ photo_album/settings_local.py
@@ -10,8 +10,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-#import dj_database_url
-#import django_heroku
+import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -43,12 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'albums'
 ]
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
-    'DEFAULT_AUTHENTICATION_CLASSES':[],
-    'DEFAULT_PERMISSION_CLASSES':[]
-}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -59,7 +54,16 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
+    'DEFAULT_AUTHENTICATION_CLASSES':[],
+    'DEFAULT_PERMISSION_CLASSES':[],
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    )
+}
 ROOT_URLCONF = 'photo_album.urls'
 
 TEMPLATES = [
@@ -117,7 +121,7 @@ USE_L10N = True
 USE_TZ = True
 
 # Change 'default' database configuration with $DATABASE_URL.
-#DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require=True))
+DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require=True))
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -141,4 +145,4 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
